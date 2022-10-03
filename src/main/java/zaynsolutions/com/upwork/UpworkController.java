f43ebd24ee376d1dc6f40f184f1ba9cb43395315
adshafqat <adshafqat@yahoo.com>
2022-10-03 15:03:38 +0100

    Update UpworkController.java

--- src/main/java/zaynsolutions/com/upwork/UpworkController.java
+++ src/main/java/zaynsolutions/com/upwork/UpworkController.java
@@ -12,8 +12,13 @@ public class UpworkController {
 	public String returnResponse() {
 		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");  
 		LocalDateTime now = LocalDateTime.now();  
-		System.out.println("This is a dummy project for upwork demo. Date:"+dtf.format(now));
-		return "This is a dummy project for upwork demo. DateTime:"+dtf.format(now);
+		System.out.println("This is a dummy project for upwork demo. Date:"+dtf.format(now));		
+				
+		int max = 5;
+        	int min = 1;
+        	int range = max - min + 1;
+		int rand = (int)(Math.random() * range) + min;
+		return "This is a test application. Random number vaule is :"+rand+" DateTime:"+dtf.format(now);
 	}
 
 	@GetMapping("/alwaysup")
